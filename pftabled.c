fba3cbeedf267a5593569b8b91988e3a51c4002e
Daniel Berredo <danielberredo@gmail.com>
2015-12-11 13:22:29 -0200

    Working on the message queue approach

--- pftabled.c
+++ pftabled.c
@@ -118,7 +118,7 @@ static void add(char *tname, struct in_addr *ip, uint8_t mask, uint32_t _timeout
                 t->timeout = time(NULL) + timeout;
                 strncpy(t->table, tname, sizeof(t->table));
                 /*TAILQ_INSERT_HEAD(&timeouts, t, queue);*/
-		memcpy(buff, t);
+		memcpy(buff, t, TIMEOUT_MESSAGE_SIZE);
 		mq_send(mqd, buff, TIMEOUT_MESSAGE_SIZE, NULL);
                 logit(LOG_NOTICE, "Included %s/%d in timeout list with timeout %d",inet_ntoa(*ip),mask, timeout);
         }
